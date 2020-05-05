17f06ad0df0b9bb00943874143979d23f28804f7
Merouane Bellaha <m.bellaha@gmail.com>
2020-05-05 16:23:58 +0200

    add switch operator and modified canAddMinusInFront

--- CountOnMe/Model/Array+manageExpression.swift
+++ CountOnMe/Model/Array+manageExpression.swift
@@ -9,20 +9,24 @@
 import Foundation
 
 extension Array where Element == String {
+
+    ///return true  if the expression contains at least 3 elements
     var haveEnoughElement: Bool {
         return self.count >= 3
     }
 
+    ///return true if the last element of the expression is not an operator or end with a point, and if  the element is not empty
     var isCorrect: Bool {
         return self.last != "+" && self.last != "-" && self.last != "x" && self.last != "/" && self.last?.last != "." && !self.isEmpty
     }
 
+    ///control if the expression contains sign equal
     var alreadyCalculated: Bool {
         return self.contains("=")
     }
 
     var canAddMinusInFront: Bool {
-        return self.last == "+" || self.last == "x" || self.last == "/" || self.isEmpty /*&& self.last?.last != "."*/
+        return self.last == "+" || self.last == "x" || self.last == "/" || self.isEmpty || (self.last == "-" && self.count >= 2 && self.reversed()[1] != "-")
     }
 
     var containsDecimal: Bool {
@@ -33,7 +37,14 @@ extension Array where Element == String {
     var containsDivisionByZero: Bool {
         for (index, element) in self.enumerated() where element == "/" && Double(self[index + 1]) == 0 {
             return true
-       }
+        }
         return false
+    }
+
+    mutating func switchTheOperator(with sign: Character, remove: Bool = false) {
+        guard var lastElement = self.last else { return }
+        if remove { lastElement.removeFirst() }
+        lastElement.insert(sign, at: lastElement.startIndex)
+        self[self.count-1] = lastElement
     }
 }
